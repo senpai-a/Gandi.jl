5485480b8ac33a44d916561619228f683404e67f
unknown <biuuuuuuuu@hotmail.com>
2020-01-12 21:17:23 +0800

    FGO UI helper functions

--- Gandi.jl
+++ Gandi.jl
@@ -3,6 +3,7 @@ module Valkyrie
 export moveTo,click,drag,dragTo,mouseDown,mouseUp
 export position,findPattern,locate
 export screenSize,pyautogui
+export waitToSee,waitToClick
 
 using PyCall
 using Images
@@ -112,17 +113,43 @@ function locate(patt::String;confidence=.9,range=(1,1,screenSize...))
 end
 
 function markPoint(img,pos)
-    x = pos[2]
-    y = pos[1]
+    y = pos[2]
+    x = pos[1]
     ret = img
     m,n=size(img)
-    t = max(y-3,1)
-    b = min(y+3,m)
-    l = max(x-3,1)
-    r = min(x+3,n)
+    t = max(x-3,1)
+    b = min(x+3,m)
+    l = max(y-3,1)
+    r = min(y+3,n)
     ret[t:b,l:r].=RGB(1,0,0)
     ret
 end
+
+function waitToClick(patt::String,range=(0,0,screenSize...),delay=0.5)
+    patt = load(patt)
+    scene = see(range)
+    loc = locate(scene,patt)
+    while isempty(loc)
+        sleep(delay)
+        scene = see(range)
+        loc = locate(scene,patt)
+    end
+    click(loc[1])
+    return nothing
+end
+
+function waitToSee(patt::String,range=(0,0,screenSize...),delay=0.5)
+    patt = load(patt)
+    scene = see(range)
+    loc = locate(scene,patt)
+    while isempty(loc)
+        sleep(delay)
+        scene = see(range)
+        loc = locate(scene,patt)
+    end
+    return loc
+end
+
 
 # drafts for testing
 #cd("C:/Users/Administrator/OneDrive/Gandi.jl/img")
@@ -136,7 +163,86 @@ end # module Valkyrie
 
 module FGOUI
 
-using Main.Valkyrie
+using Main.Valkyrie:screenSize
+export setAnchor!,pos
+export servantSkill,skillTarget,orderChange,masterSkill,card,hogu
 
+anchor = (0,0,screenSize...)
+
+function setAnchor!(x,y,w,h)
+    anchor = (x,y,w,h)
+end
+
+"""
+    pos(x,y): transfer ratio coordinate (x,y) to screen coordinate
+    where x and y are in 0~1 and pos(x,y) in [0,w]×[0,h]
+    this supports variable resolution, but anchor must be set properly with setAnchor! at first
+"""
+pos(x,y) = (anchor[1]+round(Int,x*anchor[3]),anchor[2]+round(Int,y*anchor[4]))
+
+function servantSkill(servantId,skillId)
+    if servantId∉1:3 || skillId∉1:3
+        throw(ErrorException("servantSkill: index out of range"))
+    end
+    x0,y = (52/960,(465-32)/960)
+    dx = 70/960
+    dx_servant = 238/960
+    x = x0+(servantId-1)*dx_servant+(skillId-1)*dx
+    click(pos(x,y)...)
+end
+
+function skillTarget(servantId)
+    if servantId∉1:3
+        throw(ErrorException("skillTarget: index out of range"))
+    end
+    x0,y = (254/960,(359-32)/960)
+    dx = 230
+    x = x0+(servantId-1)*dx
+    click(pos(x,y)...)
+end
+
+function orderChange(id1,id2)
+    if servantId∉1:6 || skillId∉1:6
+        throw(ErrorException("orderChange: index out of range"))
+    end
+    x0,y = (100/960,260/540)
+    dx = 150/960
+    x1 = x0+(id1-1)*dx
+    x2 = x0+(id2-1)*dx
+    click(pos(x1,y)...); sleep(.2)
+    click(pos(x2,y)...); sleep(.2)
+    click(pos(480/960,470/540)...)
+end
+
+function masterSkill(skillId)
+    if skillId∉1:3
+        throw(ErrorException("masterSkill: index out of range"))
+    end
+    click(pos(897/960,235/540)...); sleep(.2)
+    x0,y = (680/960,235/540)
+    dx = 65/960
+    x = x0 + (skillId-1)*dx
+    click(pos(x,y)...)
+end
+
+function card(cardId)
+    if cardId∉1:5
+        throw(ErrorException("card: index out of range"))
+    end
+    x0,y = (96/960,382/540)
+    dx = 192/960
+    x = x0 + (cardId-1)*dx
+    click(pos(x,y)...)
+end
+
+function hogu(hoguId)
+    if hoguId∉1:3
+        throw(ErrorException("hogu: index out of range"))
+    end
+    x0,y = (307/960,152/540)
+    dx = 170/960
+    x,y = x0+(hoguId-1)*dx
+    click(pos(x,y)...)
+end
 
 end #module FGOUI
